f4a05b7732e468fca81373deb9924fafbb8ac82c
Tran Dao Quang Chuong <chuongcongtu9x@gmail.com>
2020-03-19 20:46:30 +0700

    add collection preview and collection item

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import { Route, Switch } from 'react-router-dom';
+import ShopPage from './pages/shop/shop.component';
 
 function App() {
 
@@ -11,12 +12,17 @@ function App() {
     </div>
   )
 
+  const WomenPage = () => (
+    <div>
+      <h1> Women page</h1>
+    </div>
+  )
+
   return (
     <div>
-      {/* <HomePage /> */}
       <Switch>
         <Route exact path='/' component={HomePage} />
-        <Route path='/shop/hats' component={HashPage} />
+        <Route exact path='/shop' component={ShopPage} /> 
       </Switch>
     </div>
   );
