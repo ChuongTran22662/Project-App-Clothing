1dc7f61c7e485d741e5c8a7bac0451d0c1369853
Tran Dao Quang Chuong <chuongcongtu9x@gmail.com>
2020-03-26 15:16:14 +0700

    updating our app to redirect to home if user is signed in

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -3,9 +3,6 @@ import './menu-item.styles.scss';
 import { withRouter } from 'react-router-dom';
 
 const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => {
-
-    console.log(history)
-
     return (
         <div className={`${size} menu-item`} onClick={() => history.push(`${match.url}${linkUrl}`)}>
             <div
